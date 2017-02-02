bde381ad3f1722f53a0b782852f196563fbf8bce
Davide Cristini <davcri91@gmail.com>
2017-02-02 15:11:19 +0100

    Configured upload path.

--- softhub_site/settings.py
+++ softhub_site/settings.py
@@ -119,4 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'softhub/uploads')
+MEDIA_URL = '/softhub/uploads/'
+
 AUTH_USER_MODEL = 'softhub.User'
