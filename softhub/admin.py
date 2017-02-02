f84466b2ca53641c7b6d1a431ff25d473e033caf
Davide Cristini <davcri91@gmail.com>
2017-02-02 15:05:17 +0100

    Made migrations, runned them and using models in admin app

--- softhub/admin.py
+++ softhub/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models.OperatingSystem import OperatingSystem
+# from .models.Application import Application
+from .models.User import User
+from .models.Developer import Developer
+
+# from .models.Executable import Executable
+# from .models.Version import Version
+
+admin.site.register(User)
+admin.site.register(Developer)
+admin.site.register(OperatingSystem)
+# admin.site.register(Application)
+# admin.site.register(Executable)
+# admin.site.register(Version)
