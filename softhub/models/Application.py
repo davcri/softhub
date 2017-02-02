29b04f546990edeb1418e609439098e61fd07736
Davide Cristini <davcri91@gmail.com>
2017-02-02 15:11:39 +0100

    Added Application model.
    - Added Application model
    - Made and ran migrations
    - Added Application do Admin app

---
+++ softhub/models/Application.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+def upload_dir(app, filename):
+    '''
+        The end result will be something like:
+            "uploads/applications/Firefox/icons/firefox.png"
+    '''
+
+    path = ('applications/' + app.name + '/icons/' + filename)
+    # Note: if the file already exist in the same path, django will
+    # automatically append a random hash code to the filename!
+    return path
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    # version = models.('Version', on_delete=models.CASCADE)
+    developer = models.ForeignKey('Developer', on_delete=models.CASCADE)
+    icon = models.ImageField(upload_to=upload_dir)
+    icon.blank = True
+
+    def __str__(self):
+        return self.name
