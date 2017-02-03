852949bcaed64cdbe7ac5887282eadb0c26540db
Davide Cristini <davcri91@gmail.com>
2017-02-03 23:08:27 +0100

    Using custom User model.
    Needed to override UserCreationForm default to use Softhub
    custom User model (see Meta class).
    To understand how to do this, I needed to look at django's
    UserCreationForm source code.

--- softhub/views/UserCreation.py
+++ softhub/views/UserCreation.py
@@ -1,12 +1,13 @@
 from django.views.generic import CreateView
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from softhub.models.User import User
 from django.urls import reverse_lazy
 
 from softhub.views.UserForm import UserForm
 
 
 class UserCreation(CreateView):
-    model = User  # ?
+    model = User
     form_class = UserForm
     template_name = 'registration/user_creation.html'
     success_url = reverse_lazy('softhub:login')
--- softhub/views/UserForm.py
+++ softhub/views/UserForm.py
@@ -2,19 +2,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.forms.fields import BooleanField
 
 from softhub.models.Developer import Developer
+from softhub.models.User import User
 
 
 class UserForm(UserCreationForm):
-    developer_check = BooleanField()
+    ''' Subclass of User Creation Form
+        https://docs.djangoproject.com/en/1.10/topics/auth/default/#django.contrib.auth.forms.UserCreationForm
+    '''
+
+    # Need to override default's UserCreationForm that uses
+    # django's User in Meta class.
+    # Here we override Meta class with our custom Softhub User
+    class Meta:
+        model = User
+        fields = ("username",)
+        # field_classes = {'username': UsernameField}
+
+    developer_check = BooleanField(required=False)
 
     def save(self, commit=True):
-        user = super(UserForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user = super(UserForm, self).save(commit=True)
 
-        if commit:
-            user.save()
+        if self.cleaned_data.get('developer_check') is True:
+            Developer.objects.create(user_id=user.id)
 
-            if self.cleaned_data.get('developer_check') is True:
-                Developer.objects.create(user_id=user.id)
+        # user = super(UserForm, self).save(commit=False)
+        # user.set_password(self.cleaned_data["password1"])
+
+        # if commit:
+        #     user.save()
+        #
+        #     if self.cleaned_data.get('developer_check') is True:
+        #         Developer.objects.create(user_id=user.id)
 
         return user
