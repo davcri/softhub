76e3f5ecae3f66e8f767634dd4767e0e1dad2327
Davide Cristini <davcri91@gmail.com>
2017-02-05 17:55:40 +0100

    Removed useless code.

--- softhub/models/Developer.py
+++ softhub/models/Developer.py
@@ -2,10 +2,9 @@ from django.db import models
 # from django.contrib.auth.models import User
 from .User import User
 
+
 class Developer(models.Model):
     user = models.OneToOneField(User)
-    # name = models.CharField(max_length=200)
-    # email = models.EmailField()
 
     def __str__(self):
         return self.user.username
