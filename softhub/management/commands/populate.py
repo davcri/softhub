cf3acaab391384bcc028ad43f6f0791db8c2b607
Davide Cristini <davcri91@gmail.com>
2017-03-26 18:08:30 +0200

    Improved populate command

--- softhub/management/commands/populate.py
+++ softhub/management/commands/populate.py
@@ -1,29 +1,75 @@
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+
 from softhub.models.OperatingSystem import OperatingSystem
 from softhub.models.User import User
-
-from django.utils import timezone
+from softhub.models.License import License
 
 
 class Command(BaseCommand):
-    args = '<foo bar ...>'
-    help = 'Populates the db with common initial values'
+    args = "<foo bar ...>"
+    help = "Populates the db with common initial values"
+    verbose = False
 
     def handle(self, *args, **options):
+        if options['verbosity'] in (2, 3):
+            self.verbose = True
+
         self.create_os()
         self.create_superuser()
+        self.create_common_users()
+        self.create_licenses()
+
+    def printWarning(self, obj1, exception):
+        self.stderr.write("WARNING:" + str(obj1) + " not created.")
+
+        if self.verbose:
+            self.stdout.write(str(exception))
+
+    def create_licenses(self):
+        for key, license in License.choices:
+            try:
+                license = License.objects.create(license=license)
+                print("License created", license)
+            except Exception as e:
+                self.printWarning(license, e)
 
     def create_superuser(self):
         # TODO add check
         username = "daenn"
         email = "davcri91@gmail.com"
-        password = "django1234"
+        password = "django12"
 
-        if not User.objects.filter(username=username):
-            admin = User.objects.create_superuser(username, email, password)
+        try:
+            admin = User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password)
             print("superuser created:", admin)
-        else:
-            print("ERROR: no user created.", username, "already exist")
+        except Exception as e:
+            self.printWarning(username, e)
+
+    def create_common_users(self):
+        usernames = ["Google", "Mozilla"]
+        emails = ["google@gmail.com", "moz@mail.com"]
+        pwd = "django12"
+
+        data = list(zip(usernames, emails))
+
+        for (u, m) in data:
+            try:
+                user = User(username=u, email=m)
+                user.save()
+            except Exception as e:
+                self.printWarning(u, e)
+
+        # try:
+        #     User
+        # if not User.objects.filter(username=username):
+        #     admin = User.objects.create_superuser(username, email, password)
+        #     print("superuser created:", admin)
+        # else:
+        #     print("ERROR: no user created.", username, "already exist")
 
     def create_os(self):
         os_list = []
@@ -50,8 +96,10 @@ class Command(BaseCommand):
         os_list.append(windows7)
 
         for os in os_list:
-            if not OperatingSystem.objects.filter(name=os.name):
+            try:
+                if OperatingSystem.objects.filter(name=os.name):
+                    raise Exception("Operating system already exist")
                 os.save()
                 print("OS created", os)
-            else:
-                print("ERROR: no os created.", os, "already exist")
+            except Exception as e:
+                self.printWarning(os, e)
