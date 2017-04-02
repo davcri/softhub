97c0f6f735d65cbb5600b0f35c69da1ea1694c72
Davide Cristini <davcri91@gmail.com>
2017-04-02 10:01:13 +0200

    Improved code readability and fixed an issue.
    Issue: PermissionDenied was not working

--- softhub/views/ApplicationUpload.py
+++ softhub/views/ApplicationUpload.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.core.exceptions import PermissionDenied
 
 from softhub.models.Application import Application
 from softhub.views.ApplicationForm import ApplicationForm
@@ -15,20 +16,9 @@ class ApplicationUpload(CreateView):
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        # TODO Note that this should be always True, because of the
-        # method_decorator
-        if request.user.is_authenticated:
-            self.user = request.user
-        else:
-            pass  # Trigger some exception?
-
         # if somebody try to access this URL/view but is not a developer
-        if not self.user.isDeveloper():
-            # uncomment the next line to return only a forbidden page with
-            # http code 403
-            #
-            # self.raise_exception = True
-            return self.handle_no_permission()
+        if not self.request.user.isDeveloper():
+            raise PermissionDenied()
         else:
             return super(CreateView, self).dispatch(request, *args, **kwargs)
 
@@ -36,6 +26,8 @@ class ApplicationUpload(CreateView):
         """ Override method to pass user_id parameter to the form
         """
         form_class = self.get_form_class()
-        form = form_class(**self.get_form_kwargs(), user_id=self.user.id)
+        form = form_class(
+            user_id=self.request.user.id,
+            **self.get_form_kwargs())
 
         return form
