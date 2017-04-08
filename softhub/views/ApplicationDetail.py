ab313c75ea8297d59aff43911902b82217c27c1d
Davide Cristini <davcri91@gmail.com>
2017-04-08 21:54:01 +0200

    Application downloads improved

--- softhub/views/ApplicationDetail.py
+++ softhub/views/ApplicationDetail.py
@@ -13,14 +13,29 @@ class ApplicationDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ApplicationDetail, self).get_context_data(**kwargs)
 
-        # Get the app ID
-        app_id = self.get_object().id
+        app = self.get_object()
+
         # an alternate method is to get the URL parameter, as explained here:
         # https://stackoverflow.com/questions/15754122/url-parameters-and-logic-in-django-class-based-views-templateview#15754497
         # app_id = self.kwargs['pk']
 
-        context['versions'] = Version.objects.filter(application_id=app_id)
-        context['executables'] = Executable.objects.filter(
-            version__application_id=app_id)
+        # context['versions'] = Version.objects.filter(application_id=app.id)
+        # context['executables'] = Executable.objects.filter(
+        # version__application_id=app.id)
+
+        exes = app.get_latest_executables()
+        os_exe_dict = {}
+        for e in exes:
+            if e.release_platform.family == 'linux':
+                os_exe_dict['linux'] = e
+            elif e.release_platform.family == 'osx':
+                os_exe_dict['osx'] = e
+            elif e.release_platform.family == 'windows':
+                os_exe_dict['windows'] = e
+        print(os_exe_dict)
+
+        context['latest_executables'] = os_exe_dict
+
+        # context['other_executables'] =
 
         return context
