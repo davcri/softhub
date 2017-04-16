9b506601cde234709dae4453d4602b5823240b77
Davide Cristini <davcri91@gmail.com>
2017-04-16 22:58:28 +0200

    Implemented categories and visualization in SofthubIndex

--- softhub/views/SofthubIndex.py
+++ softhub/views/SofthubIndex.py
@@ -7,6 +7,16 @@ from softhub.models.Application import Application
 class SofthubIndex(TemplateView):
     template_name = 'softhub/index/index.html'
 
+    def __init__(self, **kwargs):
+        self.category = None
+        super().__init__(**kwargs)  # __init__ defined in View class
+
+    def dispatch(self, request):
+        category_name = self.request.GET.get('category')
+        if category_name:
+            self.category = Category.objects.get(name=category_name)
+        return super().dispatch(request)
+
     def get_context_data(self, **kwargs):
         context = super(SofthubIndex, self).get_context_data(**kwargs)
 
@@ -17,7 +27,15 @@ class SofthubIndex(TemplateView):
                 Application.objects.filter(category=c))
             categories_apps_dict[c] = app_count_for_category
 
-        context['categories_apps_dict'] = categories_apps_dict
-        context['apps'] = Application.objects.all()
+        context = {
+            'categories_apps_dict': categories_apps_dict
+        }
+
+        if self.category is not None:
+            context['current_category'] = self.category
+            context['apps'] = Application.objects.filter(
+                category=self.category)
+        else:
+            context['apps'] = Application.objects.all()
 
         return context
