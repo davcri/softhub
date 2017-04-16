8b0662308134fc5d2c30ed7ce59a8424522b3638
Davide Cristini <davcri91@gmail.com>
2017-04-16 19:28:17 +0200

    Landing page simplified.
    Added `max_apps_to_show` attribute.

--- softhub_site/views/LandingPage.py
+++ softhub_site/views/LandingPage.py
@@ -5,8 +5,19 @@ from softhub.models.Application import Application
 
 class LandingPage(TemplateView):
     template_name = "landing-page/index.html"
+    max_apps_to_show = 9
 
     def get_context_data(self, **kwargs):
         context = super(LandingPage, self).get_context_data(**kwargs)
-        context['apps'] = Application.objects.all()
+
+        # Shows a row of application only if there are 3 apps per row
+        # This prevents to show incomplete rows
+        apps = Application.objects.all()
+        appCount = len(apps)
+        fullRowsCount = appCount//3  # integer division
+        if appCount < self.max_apps_to_show:
+            context['apps'] = apps[:fullRowsCount*3]
+        else:
+            context['apps'] = apps[:self.max_apps_to_show]
+
         return context
