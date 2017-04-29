32769f91da1343c795999daa765a7100056ad4c1
Davide Cristini <davcri91@gmail.com>
2017-04-29 17:45:04 +0200

    Working on user profile

--- softhub/views/UserProfile.py
+++ softhub/views/UserProfile.py
@@ -1,5 +1,19 @@
 from django.views.generic import TemplateView
 
+from softhub.models.Application import Application
+from softhub.models.Developer import Developer
+
 
 class UserProfile(TemplateView):
     template_name = 'softhub/user/user.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        user = self.request.user  # user is a SimpleLazyObject
+        if user.isDeveloper():
+            dev = Developer.objects.get(user_id=user)
+            apps = Application.objects.filter(developer=dev)
+            context['apps'] = apps
+
+        return context
