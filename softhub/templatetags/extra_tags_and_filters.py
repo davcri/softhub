23340f06c2258a5839f8772c78c405f7c0401a66
Davide Cristini <davcri91@gmail.com>
2017-04-29 18:34:02 +0200

    Better organization for "rating.html" template. Updated references

--- softhub/templatetags/extra_tags_and_filters.py
+++ softhub/templatetags/extra_tags_and_filters.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter(name='times')
 def times(number):
     return range(1, number+1)
+
+
+@register.filter(name='to_int')
+def to_int(number):
+    return round(number)
